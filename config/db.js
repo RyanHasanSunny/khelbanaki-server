be0a949bc93314cee5766e82a1f27833da0eb74f
Ryan Hasan Sunny <ryanhasansunny31@gmail.com>
2025-05-11 11:05:53 +0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -9,7 +9,8 @@ const mongodb = async () => {
         await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB connected: ${mongoose.connection.host}`.green.bold);
     } catch (error) {
-        console.error('MongoDB connection error:', error.red.bold);
+        console.error(`MongoDB connection error: ${error.message}`.red.bold);
+        process.exit(1); // Exit the process with failure
     }
 }
 
